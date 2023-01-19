c9ac5fe35ac697562ba70e541bf6ad36e1edf879
Yan Kalinovskiy <112379732+Yan-Kalinovskiy@users.noreply.github.com>
2023-01-19 23:00:54 +0100

    Save basic files

---
+++ js/01-gallery.js
@@ -0,0 +1,10 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+console.log(galleryItems);
+
+/* 
+1. створити розмітку (Репета 1 - 1:03:04)
+2. рендерінг з рядка (1:08:13) 
+3. делегування (1:10:46)
+*/
